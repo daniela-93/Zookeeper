a9c351b677ac177383cc21b39282508f9293ec87
daniela <daniela.acuna93@gmail.com>
2021-02-01 23:40:33 -0600

    attempting to add heroku

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -3,6 +3,7 @@ const $animalForm = document.querySelector('#animal-form');
 const handleAnimalFormSubmit = event => {
   event.preventDefault();
 
+  
   // get animal data and organize it
   const name = $animalForm.querySelector('[name="animal-name"]').value;
   const species = $animalForm.querySelector('[name="species"]').value;
@@ -26,6 +27,25 @@ const handleAnimalFormSubmit = event => {
   }
   const animalObject = { name, species, diet, personalityTraits };
 
-};
+
+fetch('/api/animals', {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(animalObject)
+})
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    alert('Error: ' + response.statusText);
+  })
+  .then(postResponse => {
+    console.log(postResponse);
+    alert('Thank you for adding an animal!');
+  });
+}; 
 
 $animalForm.addEventListener('submit', handleAnimalFormSubmit);
